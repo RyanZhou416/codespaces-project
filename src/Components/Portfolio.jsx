2b44c006bda35752cef6e2f194ba9c38076588b5
Xiran Zhou <132639254+RyanZhou416@users.noreply.github.com>
2023-12-15 02:59:50 +0000

    1

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -62,7 +62,7 @@ const Portfolio = () => {
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-        <img src={image} style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideIn" }} />
+          <img src={image} style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideIn" }} />
         </div>
         <div className="container">
           {projectList.map((project) => (
