9dd22c7bd16dabc5fdeb84a1d0c329730d09a20b
Xiran Zhou <132639254+RyanZhou416@users.noreply.github.com>
2023-12-15 02:03:06 +0000

    123

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a student studying at University of Ottawa taking BS Computer Engineering.\n我是一名渥太华大学的计算机工程学生。";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "生活就像海洋，只有意志坚强的人才能到达彼岸";
 
 const About = () => {
   return (
